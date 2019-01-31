d7aa131139b560945dda38efccfd57e01acf18b5
Gonzalo Diaz <gonzalodiaz1024@gmail.com>
2019-01-31 11:45:14 -0600

    Sort tweets by created_at and limit number of tweets.

--- es_client.py
+++ es_client.py
@@ -49,27 +49,30 @@ def tweet_to_estweet(t, stored_at, sentiment=None):
     return ans
 
 
-def get_tweets(client):
+def get_tweets(client, max_tweets=5000):
     """
     Get tweets from the Elasticsearch endpoint specified by client.
 
     :param client: Elasticsearch client object.
     :return: list of tweet represented as dicts.
     """
-    search = Search(index='tweets').using(client).query("match_all")
+    search = Search(index='tweets').using(client).query('match_all').sort('created_at')
     search.execute()
 
     tweets = []
     for row in search.scan():
-        id_str = _get_with_default(row, 'id_str')
-        created_at = _get_with_default(row, 'created_at')
-        full_text = _get_with_default(row, 'full_text')
-        if id_str and created_at and full_text:
-            t = {
-                'id_str': id_str,
-                'created_at': created_at,
-                'full_text': full_text
-            }
-            tweets.append(t)
+        if len(tweets) <= max_tweets:
+            created_at = _get_with_default(row, 'created_at')
+            full_text = _get_with_default(row, 'full_text')
+            if created_at and full_text:
+                t = {
+                    'id_str': row.meta.id,
+                    'created_at': created_at,
+                    'full_text': full_text
+                }
+                tweets.append(t)
+
+    if len(tweets) > max_tweets:
+        tweets = tweets[:max_tweets]
 
     return tweets
