faccbe9d6d4cf3d16c71d8d45ae2c3607fdf81bc
Gonzalo Diaz <gonzalodiaz1024@gmail.com>
2019-02-08 10:59:15 -0600

    Add in-house sentiment prediction.
    bitcoin_sentiments.js:
    - Remove example tweets from UI
    - Hide textblob plot for now
    
    app.py:
    - Generate in-house sentiment and return plot to client
    
    plotting.py
    - polarity_column parameter
    
    requirements.txt
    - dill and scikit-learn
    
    sentiments.py (new):
    - unpickle in-house trained models and predict

---
+++ sentiments.py
@@ -0,0 +1,51 @@
+import dill
+import nltk
+from sklearn import base
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
+
+class TwitterPreprocessor(base.BaseEstimator, base.TransformerMixin):
+    def __init__(self, tweet_column):
+        self.tweet_column = tweet_column
+        self.tw_tokenizer = nltk.tokenize.TweetTokenizer(
+            strip_handles=True, reduce_len=True, preserve_case=False)
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        col = X[self.tweet_column]
+        col = col.apply(self._prepare_tweet)
+        return col
+
+    def _prepare_tweet(self, tweet):
+        tokens = self.tw_tokenizer.tokenize(tweet)
+        clean_tweet = ' '.join([token for token in tokens
+                                if len(token) > 1
+                                and not token.startswith('http')])
+        return clean_tweet
+
+
+def calc_sentiments(df_tweets, tweet_column='full_text'):
+    """
+    Return a sentiments series
+
+    :param df_tweets: A DataFrame containing tweets
+    :param tweet_column: column to extract tweet content from
+    :return:
+    """
+
+    tw_preprocessor = TwitterPreprocessor(tweet_column=tweet_column)
+    tfidf_vectorizer: TfidfVectorizer = dill.load(open('tfidf_vectorizer.dill', 'rb'))
+    best_mnb_estimator: MultinomialNB = dill.load(open('best_mnb_estimator.dill', 'rb'))
+
+    pipe_estimator = Pipeline([
+        ('tw_preprocessor', tw_preprocessor),
+        ('tfidf_vectorizer', tfidf_vectorizer),
+        ('best_mnb_estimator', best_mnb_estimator)
+    ])
+
+    prediction = pipe_estimator.predict(df_tweets)
+    return prediction
--- static/bitcoin_sentiments.js
+++ static/bitcoin_sentiments.js
@@ -13,10 +13,7 @@ angular.module('BitcoinSentimentsApp', [])
 .controller('TweetTableController', function($scope, BitcoinSentimentsService) {
     var tweetTable = this;
 
-    tweetTable.tweets = [
-      {'id_str': 10001012201, 'created_at': '2019-01-01 00:00:01', 'full_text': 'Tweet number 1', 'subjectivity': 0.0, 'polarity': -1.0},
-      {'id_str': 19111989189, 'created_at': '2025-03-03 01:00:01', 'full_text': 'Tweet number 2', 'subjectivity': 0.5, 'polarity': 0.8}
-    ];
+    tweetTable.tweets = [];
 
     tweetTable.showTimeline = function() {
         if($('#card_li_plot a').hasClass('bs_timeline_empty') && $('#card_li_intro a').hasClass('active')) {
@@ -34,7 +31,8 @@ angular.module('BitcoinSentimentsApp', [])
                 return response.json()
             }).then(function(data) {
                 $('#plot_card_spinner').css('display', 'none')
-                Bokeh.embed.embed_item(data['plot2'], 'mainplot_div')
+//                Bokeh.embed.embed_item(data['plot2'], 'mainplot_div')
+                Bokeh.embed.embed_item(data['ih_sentiments_plot'], 'mainplot_div')
                 tweetTable.tweets = data['tweets']
                 $scope.$apply();
             })
